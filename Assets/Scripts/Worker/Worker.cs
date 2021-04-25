f34fd92204e0f0062b272281169cb4b52be0702c
steven-vd <stevenvandorp7@gmail.com>
2021-04-25 13:55:59 +0200

    Added Worker Animations

--- Assets/Scripts/Worker/Worker.cs
+++ Assets/Scripts/Worker/Worker.cs
@@ -8,11 +8,15 @@ public class Worker : MonoBehaviour {
 	private float task_progress = 0.0f;
 	public Task task = null;
 
+	private Animator anim;
+
 	void Start() {
 		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
 	}
 
 	void Update() {
+		anim.SetBool("mining", false);
 		if (task != null) {
 			float dir = task.pos.x - transform.position.x;
 			const float work_range = 0.3f;
@@ -30,6 +34,7 @@ public class Worker : MonoBehaviour {
 				}
 			} else {
 				//Work on task
+				anim.SetBool("mining", true);
 				const float task_completion_speed = 1.0f;
 				task_progress += task_completion_speed * Time.deltaTime;
 				if (task.completion_time <= task_progress) {
@@ -38,10 +43,21 @@ public class Worker : MonoBehaviour {
 					task = null;
 				}
 			}
+		} else {
+			transform.rotation = Quaternion.Euler(0, 0, 0);
+			anim.SetBool("walking", false);
 		}
 	}
 
 	private void move(int dir) {
+		// - Walking Animation -
+		anim.SetBool("walking", true);
+		if (dir > 0) {
+			transform.rotation = Quaternion.Euler(0, -33, 0);
+		} else {
+			transform.rotation = Quaternion.Euler(0, 33, 0);
+		}
+
 		const float speed = 3f;
 		//transform.Translate(new Vector3(dir * speed * Time.deltaTime, 0, 0));
 		//rb.MovePosition(new Vector2(dir * speed * Time.deltaTime, 0));
